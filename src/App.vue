7bde19985e5159ddef63826389201f2efd3daa1e
Clara Jacintho <mcmachry@gmail.com>
2020-02-21 01:13:10 +0100

    Added date picker, added conversion function to AUC

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Roman Calendar Converter</h1>
+    <p>Convert from the Gregorian Calandar to the roman dating system. More information on <a href="https://en.wikipedia.org/wiki/Roman_calendar">Wikipedia</a> </p>
+    <div id="datepicker">
+      <datepicker :inline="true" @selected="convert" format="dd/MM/yyyy"></datepicker>
+    </div>
+    <h2>{{this.date}}</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Datepicker from 'vuejs-datepicker'
+import romanCalendar from './assets/kalendae_romani.json'
+import translations from './assets/calendar_translations.json'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Datepicker
+  },
+  data () {
+    return {
+      date: ''
+    }
+  },
+  methods: {
+    convert (date) {
+      const month = date.getMonth() + 1
+      const day = date.getDate()
+      const year = date.getFullYear()
+      const romanYear = year + 753
+      let festivity = ''
+      let romanMonth = translations.monthNames[month]
+      let romanDay = ''
+      for (const monthSchema of romanCalendar) {
+        if (monthSchema.months.includes(month)) {
+          if (day === 1) {
+            festivity = 'Kalendis'
+          } else if (day === monthSchema.nonis) {
+            festivity = 'Nonis'
+          } else if (day === monthSchema.idibus) {
+            festivity = 'Idibus'
+          } else if (day < monthSchema.nonis) {
+            festivity = 'Nonas'
+            romanDay = translations.dayNames[monthSchema.nonis - day]
+          } else if (day < monthSchema.idibus) {
+            festivity = 'Idus'
+            romanDay = translations.dayNames[monthSchema.idibus - day]
+          } else {
+            festivity = 'Kalendas'
+            let totalDays
+            if (month === 2 && this.isLeapYear(year)) {
+              totalDays = 29
+            } else {
+              totalDays = monthSchema.days
+            }
+            romanDay = translations.dayNames[totalDays - day + 1]
+            romanMonth = translations.monthNames[month + 1]
+          }
+          break
+        }
+      }
+      this.date = romanDay + ' ' + festivity + ' ' + romanMonth + ' Ab Urbe Condita ' + romanYear
+    },
+    isLeapYear (year) {
+      // eslint-disable-next-line
+      return !((year % 4) && (year % 100) || !(year % 400));
+    }
   }
 }
 </script>
@@ -22,7 +76,14 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ca5959;
   margin-top: 60px;
+  height: 100vh
+}
+
+#datepicker {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
